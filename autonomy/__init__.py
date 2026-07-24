0ea6c23752ed68291574ee0b49954d6cb77eeac7
Christian Lejeune <leje.chris@gmail.com>
2026-07-24 10:48:51 -0500

    feat: export GitHub adapter primitives

--- autonomy/__init__.py
+++ autonomy/__init__.py
@@ -1,6 +1,25 @@
 """Autonomous engineering platform v2 foundation."""
 
 from .contracts import Job, JobState, RepositoryTarget
+from .github_adapter import (
+    DryRunMutationExecutor,
+    GitHubAdapter,
+    GitHubAdapterError,
+    GuardedMutationExecutor,
+    MutationCommand,
+    MutationKind,
+)
 from .store import JsonJobStore
 
-__all__ = ["Job", "JobState", "RepositoryTarget", "JsonJobStore"]
+__all__ = [
+    "DryRunMutationExecutor",
+    "GitHubAdapter",
+    "GitHubAdapterError",
+    "GuardedMutationExecutor",
+    "Job",
+    "JobState",
+    "JsonJobStore",
+    "MutationCommand",
+    "MutationKind",
+    "RepositoryTarget",
+]
